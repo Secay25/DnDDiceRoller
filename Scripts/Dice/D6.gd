9e45ad7774cdfbaceb68b979f286190d43cc2b83
Secay25 <M.Al-Amri.S@outlook.com>
2024-05-30 16:29:07 +0200

    Added the D6.

---
+++ Scripts/Dice/D6.gd
@@ -0,0 +1,9 @@
+class_name D6 extends Dice
+
+func _onDiePressed(maxNumber: int) -> void:
+	#ALERT Godot stopped recognizing the inherited method and I couldn't actually connect the signal
+	super(maxNumber)
+
+func _onSwatchButtonPressed(swatchButton: int) -> void:
+	#ALERT Godot stopped recognizing the inherited method and I couldn't actually connect the signal
+	super(swatchButton)
