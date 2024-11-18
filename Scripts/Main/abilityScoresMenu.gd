005893828f72dcc5f93bffdfb7a8a8b4d41c40ae
Secay <129698157+Secay25@users.noreply.github.com>
2024-11-18 16:24:46 +0100

    Added the dice to the subwindow and the functionality to roll and apply color based on the values.

--- Scripts/Main/abilityScoresMenu.gd
+++ Scripts/Main/abilityScoresMenu.gd
@@ -2,10 +2,27 @@ extends Control
 
 @export var otherGroups: Array[Control]
 @export var backButton: Button
+@onready var labels: Array[Label] = [
+	$Dice/TopLeftDie/TopLeftLabel,
+	$Dice/TopRightDie/TopRightLabel,
+	$Dice/BottomRightDie/BottomRightLabel,
+	$Dice/BottomLeftDie/BottomLeftLabel,
+	]
+@onready var history: Control = $"../Settings/History"
 var isInMenu: bool = false
 var isTweening: bool = false
 const ROLLINGDURATION: float = .5 # in seconds
 const INACTIVEPOSY: float = 1024.0
+const RED: Color = Color.FIREBRICK
+const BLUE: Color = Color.NAVY_BLUE
+const GREEN: Color = Color.DARK_GREEN
+const GRAY: Color = Color.DIM_GRAY
+
+func _ready() -> void:
+	$Dice/TopLeftDie.self_modulate = RED
+	$Dice/TopRightDie.self_modulate = BLUE
+	$Dice/BottomRightDie.self_modulate = GREEN
+	$Dice/BottomLeftDie.self_modulate = GRAY
 
 func SwitchOtherGroups(isVisible: bool = false) -> void:
 	for i in range(len(otherGroups)):
@@ -31,3 +48,36 @@ func _onAbilityScoresPressed() -> void:
 			isTweening = false
 			backButton.disabled = false
 			)
+
+func _onDiePressed() -> void:
+	var rolledNumbers: Array[int] = [0,0,0,0]
+	var redUsed: bool = false
+	var blueUsed: bool = false
+	var totalAmount: int = 0
+	
+	for i: int in range(rolledNumbers.size()):
+		rolledNumbers[i] = randi_range(1,6)
+		labels[i].text = str(rolledNumbers[i])
+	
+	var lowestNumber: int = rolledNumbers.min()
+	var lowestLabel: Label
+	
+	for i: int in range(rolledNumbers.size()):
+		if labels[i].text == str(lowestNumber):
+			lowestLabel = labels[i]
+		
+		totalAmount += rolledNumbers[i]
+	
+	totalAmount -= lowestNumber
+	lowestLabel.get_parent().self_modulate = GRAY
+	
+	for label: Label in labels:
+		if label != lowestLabel:
+			if !redUsed:
+				label.get_parent().self_modulate = RED
+				redUsed = true
+			elif !blueUsed:
+				label.get_parent().self_modulate = BLUE
+				blueUsed = true
+			else:
+				label.get_parent().self_modulate = GREEN
